ac063fec861b5df5d16a7b5fe60984380cf42cb2
Phuong <phuong74200@gmail.com>
2020-11-11 20:50:06 +0700

    none

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, { wsEngine: 'ws' });
 const path = require('path')
+const seedrandom = require('seedrandom')
+let db = require('nedb');
 
-const PORT = 443;
+db.messages = new db(path.join(`${__dirname}/data/database/message.db`));
+db.messages.loadDatabase();
+
+console.log(seedrandom.alea('123').int32())
+
+const PORT = 3000;
+
+let messageStorage = [];
 
 app.use(express.static('public'));
 
@@ -14,15 +23,51 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
+let users = {};
 
 io.on('connect', (socket) => {
-    console.log('socket connected');
     //console.log(socket.handshake)
+
+    function sendMessage(data) {
+        io.emit('server-send', `#${users[socket.id]} - ${data}`)
+        db.messages.insert({
+            userid: users[socket.id],
+            message: data,
+            id: ""
+        })
+        messageStorage.push(data)
+    }
+
     socket.on('client-send', (data) => {
-        console.log('recive')
-        if(data.trim() == '') return false;
-        io.emit('server-send', data)
+        if (data.trim() == '') return false;
+        io.emit('server-send', `#${users[socket.id]} - ${data}`)
+        db.messages.insert({
+            userid: users[socket.id],
+            message: data,
+            _id: ""
+        })
+        messageStorage.push(data)
     })
+
+    db.messages.find({}, (err, docs) => {
+        socket.emit('server-load', docs)
+    })
+
+    socket.on('login', function (data) {
+        data.userId = Math.abs(seedrandom(data.userId).int32()).toString().slice(0, 5);
+        users[socket.id] = data.userId;
+        console.log(data.userId + ' connected');
+        io.emit('server-send', `#${users[socket.id]} has joined in`)
+        messageStorage.push(`#${users[socket.id]} has joined in`)
+        users[socket.id] = data.userId;
+    });
+
+    socket.on('disconnect', function () {
+        console.log('user ' + users[socket.id] + ' disconnected');
+        io.emit('server-send', `#${users[socket.id]} has leave`)
+        messageStorage.push(`#${users[socket.id]} has leave`)
+        delete users[socket.id];
+    });
 })
 
 http.listen(PORT, () => {
--- public/data/js/main.js
+++ public/data/js/main.js
@@ -1,15 +1,27 @@
 const socket = io('ws://localhost:3000');
 
-socket.on('connect', () => {
+function appendMessage(data) {
+    let message = document.createElement('div');
+    message.className = 'message';
+    message.textContent = data;
+    document.getElementById('comeMessage').appendChild(message)
+    document.getElementById('comeMessage').scrollTop = document.getElementById('comeMessage').scrollHeight;
+}
+
+socket.on('connect', (sdata) => {
+    console.log(socket.id)
+    socket.emit('login', { userId: socket.id });
     console.log('socket io listen 3000')
     socket.on('server-send', data => {
+        appendMessage(data)
+    });
+    socket.on('server-load', data => {
         console.log(data)
-        let message = document.createElement('div');
-        message.className = 'message';
-        message.textContent = data;
-        document.getElementById('comeMessage').appendChild(message)
-    })
-
+        for (let mes of data) {
+            appendMessage(`#${mes.userid} - ${mes.message}`)
+        }
+        appendMessage(`Do not share your personal data with anyone! Don't trust anyone in there :v`)
+    });
 });
 
 document.getElementById('message').addEventListener('keyup', (e) => {
